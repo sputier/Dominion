8cacdd3c6b7d72a6ad53bb552024b2bc1b971d5c
Sebastien P <sputier@gmail.com>
2016-04-18 11:51:57 +0200

    Initial commit

---
+++ src/Dominion/Scripts/game/interfaces.ts
@@ -0,0 +1,48 @@
+﻿
+export interface ICard {
+    id: string;
+    name: string;
+    extensionSet: ExtensionSet;
+    cost: ICardCost;
+    type: CardType[];
+    capabilities: ICardCapabilities;
+}
+
+export enum CardType {
+    action,
+    reaction,
+    treasure,
+    duration,
+    attack,
+    victory,
+    looter,
+    ruins,
+    knight,
+    shelter
+}
+
+export enum ExtensionSet {
+    base,
+    intrigue,       //intrigue
+    seaside,        //rivages
+    alchemy,        //alchmie
+    prosperity,     //prosperite
+    cornucopia,     //abondance
+    hinterlands,    //larrierepays
+    dark_ages,      //agedestenebres
+    guilds,         //guildes
+    adventures,     //aventures
+    promotional
+}
+
+export interface ICardCost {
+    money: number;
+    potion: number;
+}
+
+export interface IPlayer {
+    name: string;
+    gameHistory: {
+        
+    }
+}
